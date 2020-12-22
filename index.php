f4602f844cea91badb20ecbc848f47ec2cb4bae1
uzume <uzume@example.com>
2020-12-22 14:16:43 +0100

    Use strict xhtml1 dtd

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
-<!DOCTYPE html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+       "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html lang="fr">
     <head>
-        <meta charset="utf-8">
+        <meta charset="utf-8" />
 
-        <title>My website</title>
+        <title>Techno Web – Blog</title>
+        <link rel="stylesheet" href="/dist/main.css" />
     </head>
     <body>
         <h1>Projet web</h1>
