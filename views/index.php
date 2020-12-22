124c9e6d34bcaaf5efde7cf2d5b72aa01dab42dd
uzume <uzume@example.com>
2020-12-22 22:23:34 +0100

    Use responsive card in index view

--- views/index.php
+++ views/index.php
@@ -1,20 +1,21 @@
 <div class="container">
     <h1>Blog techno web</h1>
 
-    <div class="card-deck">
+    <div class="row">
 
-    <?php foreach($posts as $post): ?>
+        <?php foreach($posts as $post): ?>
 
-        <div class="card">
-            <img class="card-img-top" src="/dist/img/<?= $post['preview']; ?>" />
-            <div class="card-body">
-                <h5 class="card-title"><?= $post['name']; ?></h5>
-                <p class="card-text"><?= $post['description']; ?></p>
+        <div class="col-12 col-md-6 col-lg-3">
+            <div class="card">
+                <img class="card-img-top" src="/dist/img/<?= $post['preview']; ?>" alt="<?= $post['name']; ?>/>
+                <div class="card-body">
+                    <h5 class="card-title"><?= $post['name']; ?></h5>
+                    <p class="card-text"><?= $post['description']; ?></p>
+                </div>
             </div>
         </div>
 
-    <?php endforeach; ?>
+        <?php endforeach; ?>
 
     </div>
-
 </div>
