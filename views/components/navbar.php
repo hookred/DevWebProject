6e3b8b059daa41f9a5ce3116432f4ffd23a90844
uzume <uzume@example.com>
2020-12-22 21:19:11 +0100

    Use bootstrap JavaScript and create nav

--- views/components/navbar.php
+++ views/components/navbar.php
@@ -1,3 +1,21 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <a class="navbar-brand" href="/">TechBlog</a>
+
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="/">Accueil</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/post">Article parlant de Docker</a>
+            </li>
+            <li class="register">
+                <a class="nav-link" href="/register">S'inscrire</a>
+            </li>
+        </ul>
+    </div>
+
 </nav>
